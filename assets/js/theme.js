8b905e8c1aebd0c10be278e868fbb31b54a56d99
MohamedRagabJr <mohamedragab201020@gmail.com>
2025-04-25 18:11:49 +0300

    responsive and arabic

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -845,79 +845,6 @@
 	// Portfolio grid categories filter
 	// =================================
 
-	// On category trigger click.
-	$(".ttgr-cat-trigger").on("click", function() {
-		$("body").addClass("ttgr-cat-nav-open");
-		if ($("body").hasClass("ttgr-cat-nav-open")) {
-
-			gsap.to(".portfolio-grid-item", { duration: 0.3, scale: 0.9 });
-			gsap.to("#page-header, #tt-header, .ttgr-cat-trigger", { duration: 0.3, autoAlpha: 0 });
-
-			// Make "ttgr-cat-nav" unclickable.
-			$(".ttgr-cat-nav").off("click");
-
-			// Catecories step in animations.
-			let tl_ttgrIn = gsap.timeline({
-				// Wait until the timeline is completed then make "ttgr-cat-nav" clickable again.
-				onComplete: function() {  
-					ttCatNavClose();
-					
-					// Disable page scroll if open
-					if ($("body").hasClass("tt-smooth-scroll") && !tt_isMobile) {
-						lenis.stop();
-					} else {
-						$("html").addClass("tt-no-scroll");
-					}
-				}
-			});
-			tl_ttgrIn.to(".ttgr-cat-nav", { duration: 0.3, autoAlpha: 1 });
-			tl_ttgrIn.from(".ttgr-cat-close-btn", { duration: 0.3, y: 10, autoAlpha: 0, ease: Power2.easeIn });
-			tl_ttgrIn.from(".ttgr-cat-list > li", { duration: 0.3, y: 40, autoAlpha: 0, stagger: 0.07, ease: Power2.easeOut, clearProps:"all" }, "-=0.2");
-
-			// On catecory link hover
-			$(".ttgr-cat-list").on("mouseenter", function() {
-				$(this).parents(".ttgr-cat-nav").addClass("ttgr-cat-nav-hover");
-			}).on("mouseleave", function() {
-				$(this).parents(".ttgr-cat-nav").removeClass("ttgr-cat-nav-hover");
-			});
-
-		}
-	});
-
-	// On close click function
-	function ttCatNavClose() {
-		const $ttgrCatNavList = $(".ttgr-cat-list");
-
-		// Close nav when clicking outside the ".ttgr-cat-list"
-		$(".ttgr-cat-nav, .ttgr-cat-close-btn").on("click", function (e) {
-			if ($("body").hasClass("ttgr-cat-nav-open") && !$ttgrCatNavList.is(e.target) && $ttgrCatNavList.has(e.target).length === 0) {
-				
-				$("body").removeClass("ttgr-cat-nav-open");
-
-				// Catecories step out animations
-				let tl_ttgrClose = gsap.timeline();
-					tl_ttgrClose.to(".ttgr-cat-close-btn", { duration: 0.3, y: -10, autoAlpha: 0, ease: Power2.easeIn });
-					tl_ttgrClose.to(".ttgr-cat-list > li", { duration: 0.3, y: -40, autoAlpha: 0, stagger: 0.07, ease: Power2.easeIn }, "-=0.3");
-					tl_ttgrClose.to(".ttgr-cat-nav", { duration: 0.3, autoAlpha: 0, clearProps:"all" }, "+=0.2");
-					tl_ttgrClose.to(".portfolio-grid-item", { duration: 0.3, scale: 1, clearProps:"all" }, "-=0.4");
-					tl_ttgrClose.to("#page-header, #tt-header, .ttgr-cat-trigger", { duration: 0.3, autoAlpha: 1, clearProps:"all" }, "-=0.4");
-					tl_ttgrClose.to(".ttgr-cat-list > li, .ttgr-cat-close-btn", { clearProps:"all" }); // clearProps only
-
-				// Enable page scroll if closed
-				if ($("body").hasClass("tt-smooth-scroll") && !tt_isMobile) {
-					lenis.start();
-				} else {
-					$("html").removeClass("tt-no-scroll");
-				}
-
-			}
-
-			// Refresh ScrollTrigger
-			ScrollTrigger.refresh();
-		});
-	}
-
-
 
 	// ================================================================
 	// Portfolio compact list
@@ -1611,40 +1538,7 @@
 
 
 
-	// ================================================================
-	// Accordion
-	// ================================================================
 
-	$(".tt-accordion").each(function() {
-		let $ttAccordion = $(this);
-
-		// If accordion content has class "is-open"
-		$ttAccordion.find(".tt-accordion-item").each(function() {
-			let $ttAccItem = $(this);
-
-			if ($ttAccItem.find(".tt-accordion-content").hasClass("is-open")) {
-				$ttAccItem.addClass("active");
-			}
-		});
-
-		// Accordion item on click
-		$ttAccordion.find(".tt-accordion-heading").on("click", function() {
-			let $ttAccHeading = $(this);
-			let $ttAccItem = $ttAccHeading.parents(".tt-accordion-item");
-			let $ttAccContent = $ttAccHeading.next(".tt-accordion-content");
-
-			if ($ttAccItem.hasClass("active")) {
-				$ttAccItem.removeClass("active");
-				$ttAccContent.slideUp(350);
-			} else {
-				$ttAccordion.find(".tt-accordion-item.active").removeClass("active")
-				.find(".tt-accordion-content").slideUp(350);
-				$ttAccItem.addClass("active");
-				$ttAccContent.slideDown(350);
-			}
-			return false;
-		});
-	});
 
 
 
@@ -1720,14 +1614,12 @@
 	// ================================================================
 	// Forms
 	// ================================================================
-
 	// Remove placeholder on focus
 	$('input:not([type="checkbox"]):not([type="radio"]), textarea').focus(function () {
 		$(this).data("placeholder", $(this).attr("placeholder")).attr("placeholder", "");
 	}).blur(function () {
 		$(this).attr("placeholder", $(this).data("placeholder"));
 	}); 
-
 	// If <form> has class "tt-form-creative"
 	if ($("form").hasClass("tt-form-creative")) {
 		// Add class "tt-fg-typing" if typing 
@@ -1753,30 +1645,6 @@
 			if( log ) alert(log);
 		}
 	});
-
-
-	// Universal PHP Mail Feedback Script 
-	// (https://github.com/agragregra/uniMail)
-	// ========================================
-
-	// E-mail Ajax Send
-	$("#tt-contact-form").submit(function() { // your contact form ID
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", // mail.php path
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you. Your message has been sent!");
-			setTimeout(function() {
-			// Done Functions
-			th.trigger("reset");
-			}, 800);
-		});
-		return false;
-	});
-
-
 
 	// ================================================================
 	// Scroll between anchors 
